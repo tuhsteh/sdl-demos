8ddcfa69e19b05af543aa435bf9b1878bdcace78
tstear-on-ladder <t.stear@gmail.com>
2021-04-15 20:38:07 -0600

    brace style again

--- sdl-hello-world-random/src/sdl-hello-world.cpp
+++ sdl-hello-world-random/src/sdl-hello-world.cpp
@@ -1,50 +1,51 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <SDL2/SDL.h>
+#include<SDL2/SDL.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include<iostream>
 
-int main(int argv, char** args) {
-    time_t t;
+int main(int argv, char **args) {
+  time_t t;
 
-    srand((unsigned) time(&t));
-	SDL_Init(SDL_INIT_EVERYTHING);
+  srand((unsigned)time(&t));
+  SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
-	bool isRunning = true;
-	SDL_Event event;
+  bool isRunning = true;
+  SDL_Event event;
 
-	while (isRunning)	{
-		int R = rand() % 255;
-		int B = rand() % 255;
-		int G = rand() % 255;
+  while (isRunning) {
+    int R = rand() % 255;
+    int B = rand() % 255;
+    int G = rand() % 255;
 
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			case SDL_QUIT:
-				isRunning = false;
-				break;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          isRunning = false;
+          break;
 
-			case SDL_KEYDOWN:
-				if (event.key.keysym.sym == SDLK_ESCAPE) {
-					isRunning = false;
-				}
-			}
-		}
+        case SDL_KEYDOWN:
+          if (event.key.keysym.sym == SDLK_ESCAPE) {
+            isRunning = false;
+          }
+      }
+    }
 
-		usleep(1000000 + 1000000*(rand() % 5));
+    usleep(1000000 + 1000000 * (rand() % 5));
 
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, R, B, G, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, R, B, G, 255);
 
-		SDL_RenderPresent(renderer);
-	}
+    SDL_RenderPresent(renderer);
+  }
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-	return 0;
+  return 0;
 }
