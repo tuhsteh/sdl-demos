f2a58e1cafe81235bf27ae58c5b951671d133390
tstear-on-ladder <t.stear@gmail.com>
2021-04-18 17:56:21 -0600

    Close button demo

---
+++ sdl-close-button/src/sdl-close-button.cpp
@@ -0,0 +1,43 @@
+#include <SDL2/SDL.h>
+
+#include <cstdio>
+#include <iostream>
+
+int main(int argv, char **args) {
+  using namespace std;
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
+
+  SDL_Window *window = nullptr;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "Video Init Error:  " << SDL_GetError() << std::endl;
+  } else {
+    window =
+        SDL_CreateWindow("SDL CodingMadeEasy Series", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    if (NULL == window) {
+      std::cerr << "Video Init Error:  " << SDL_GetError() << std::endl;
+    } else {
+      bool isRunning = true;
+
+      SDL_Event ev;
+
+      while(isRunning) {
+        while(SDL_PollEvent(&ev) != 0) {
+          std::cout << ev.type << std::endl;
+
+          if(ev.type == SDL_QUIT) {
+            isRunning = false;
+          }
+        }
+        SDL_UpdateWindowSurface(window);
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
